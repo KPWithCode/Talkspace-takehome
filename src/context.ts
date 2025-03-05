f85d17351e31161f949f209773164e618c28a71d
Kyle Phillips <kphillips375@yahoo.com>
2025-03-05 12:50:41 -0500

    context and proper typing for avatar, avatarurl and avatar context

--- src/context.ts
+++ src/context.ts
@@ -1,6 +1,31 @@
 import { createContext} from "react"
+import { AvatarOptions, AvatarList } from "./Types"
+import { defaultRobot } from "./Services"
 
+interface AvatarContextType {
+    avatarOptions: AvatarOptions
+    setAvatarOptions:  React.Dispatch<React.SetStateAction<AvatarOptions>>;
+}
 
-export const AvatarContext = createContext({})
-export const AvatarURLContext= createContext()
-export const AvatarListContext = createContext({})
+interface AvatarListContextType {
+    avatarList: AvatarList;
+    setAvatarList: React.Dispatch<React.SetStateAction<AvatarList>>;
+    refreshList: () => void;
+    isLoading?: boolean;
+    error?: string | null;
+  }
+  
+  export const AvatarContext = createContext<AvatarContextType>({
+    avatarOptions: defaultRobot,
+    setAvatarOptions: () => {}
+  })
+  
+  export const AvatarURLContext = createContext<string>("")
+  
+  export const AvatarListContext = createContext<AvatarListContextType>({
+    avatarList: [],
+    setAvatarList: () => {},
+    refreshList: () => {},
+    isLoading: false,
+    error: null
+  })
